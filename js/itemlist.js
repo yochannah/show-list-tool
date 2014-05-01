b7f9b70ee4d75a6fcb9fd6c37bda6145b7888487
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-05-01 18:55:11 +0100

    The magical power of 12

--- js/itemlist.js
+++ js/itemlist.js
@@ -4,12 +4,12 @@ define(['react', './listitem'], function (React, ListItem) {
   var ItemList = React.createClass({
     displayName: 'ItemList',
     getInitialState: function () {
-      return {offset: 0, size: 8};
+      return {offset: 0, size: 12};
     },
     render: function () {
       var items = this.props.items.slice(this.state.offset, this.state.size).map(function (item) {
         return ListItem({
-          className: 'col-sm-3',
+          className: 'col-xs-4 col-sm-3 col-md-2',
           key: item.objectId,
           item: item
         });

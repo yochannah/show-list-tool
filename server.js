6126c47a0f1422fa289563758ade7bbb9fafbe93
Yo Yehudi <yochannah@gmail.com>
2015-10-21 10:45:02 +0100

    server will try another port if 8080 is taken

--- server.js
+++ server.js
@@ -7,5 +7,14 @@ var express = require("express");
 var app = express();
 app.use(express.static(__dirname));
 
-app.listen(PORT);
-console.log("Server started on " + PORT);
+startServer(PORT);
+
+function startServer(port) {
+  console.log('Starting server on port', port, '...');
+  app.listen(port)
+    .on('error', function(){
+      console.log('...port ' + port + " was already taken.");
+      PORT = port + 1;
+      startServer(PORT);
+    });
+}
